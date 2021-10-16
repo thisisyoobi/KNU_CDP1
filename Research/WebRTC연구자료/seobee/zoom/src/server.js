e63171bdba7c5a1b4ac76489c94453106eb8af48
seogyeongseob <syhbong9@naver.com>
2021-10-16 16:53:43 +0900

    WebSocket Messages

--- Research/WebRTC연구자료/seobee/zoom/src/server.js
+++ Research/WebRTC연구자료/seobee/zoom/src/server.js
@@ -16,10 +16,17 @@ const handleListen = () => console.log('Listenig on http://localhost:3000');
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
-function handelConnection(socket){
-    console.log(socket);
-}
-wss.on("connection", handelConnection);
+// function handelConnection(socket){
+//     console.log(socket);
+// }
+wss.on("connection", (socket) => {
+    console.log("Connected to Browser ✅");
+    socket.on("close", () => console.log("Disconnected from the Browser ❌"));
+    socket.on("message", (message) => {
+        console.log(message.toString('utf8'));
+    });
+    socket.send("hello!!");
+});
 
 server.listen(3000, handleListen);
 
