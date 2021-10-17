23aaa0f80d34f0028617e501ca8bc7dead77fc77
seogyeongseob <syhbong9@naver.com>
2021-10-17 13:38:11 +0900

    Nicknames part Two

--- Research/WebRTC연구자료/seobee/zoom/src/server.js
+++ Research/WebRTC연구자료/seobee/zoom/src/server.js
@@ -24,10 +24,18 @@ const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
     socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-    socket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message.toString('utf8')));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${socket.nickname}: ${message.payload}`));
+            case "nickname":
+                socket["nickname"] = message.payload;
+        }
+        //sockets.forEach((aSocket) => aSocket.send(message.toString('utf8')));
     });
 });
 
