de6d93b622e7e2a8a4f45fe27559e9690f711ab5
seogyeongseob <syhbong9@naver.com>
2021-10-17 20:02:11 +0900

    Room Count part Two

--- Research/WebRTC연구자료/seobee/zoom/src/server.js
+++ Research/WebRTC연구자료/seobee/zoom/src/server.js
@@ -8,8 +8,8 @@ const app = express();
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req, res) => res.render("home"));
-app.get("/*", (req,res) => res.redirect("/"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_,res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
@@ -37,9 +37,13 @@ wsServer.on("connection", (socket) => {
         socket.join(roomName);
         done();
         socket.to(roomName).emit("welcome", socket.nickname);
+        wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("disconnecting", () => {
         socket.rooms.forEach((room) => socket.to(room).emit("bye", socket.nickname));
+    });
+    socket.on("disconnect", () => {
+        wsServer.sockets.emit("room_change", publicRooms());
     });
     socket.on("new_message", (msg, room, done) => {
         socket.to(room).emit("new_message", `${socket.nickname}: ${msg}`);
