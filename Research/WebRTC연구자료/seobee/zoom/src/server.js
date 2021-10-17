3f33b8f1a67012144e95ff7b37f08554409f154c
seogyeongseob <syhbong9@naver.com>
2021-10-17 15:02:50 +0900

    SocketIO is Amazing

--- Research/WebRTC연구자료/seobee/zoom/src/server.js
+++ Research/WebRTC연구자료/seobee/zoom/src/server.js
@@ -15,7 +15,10 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        done();
+    });
 });
 
 
